f2285441a36ebc6b91bc52b30b860317554d0cb2
Pradipta Saloi <saloipradipta@gmail.com>
2020-01-02 15:05:52 +0530

    Test File added

---
+++ src/Test.java
@@ -0,0 +1,24 @@
+package infixEvaluation;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		//Create the object
+		InfixEvaluation testObj = new InfixEvaluation();
+		
+		//call the evaluate method and pass the expression as string
+		//test case 1 - 2+8-1*4/1
+		String testExpression1 = "2+8-1*4/1";
+		System.out.println(testObj.evaluate(testExpression1));
+		
+		//Lets add braces and decimals
+		//test case 2 - 4+(1.4*2.1)+44-21.2
+		System.out.println(testObj.evaluate("4+(1.4*2.1)+44-21.2"));
+		
+		/*Output of above two test cases - 
+		 * Test case 1 - 6.0
+		 * Test case 2 - 29.740000000000002
+		 * */
+	}
+}
